cd302d920c7faeb6d80a8207268699de9f9476eb
Jason Wu <jiangwu0228@gmail.com>
2022-01-28 18:35:14 +1300

    hide api key

--- src/contexts/StateContextProvider.js
+++ src/contexts/StateContextProvider.js
@@ -15,7 +15,7 @@ export const StateContextProvider = ({ children }) => {
       method: "GET",
       headers: {
         "x-rapidapi-host": "google-search3.p.rapidapi.com",
-        "x-rapidapi-key": "2a2b987a6bmsh276f5712854c62fp132404jsnaa3c34cb6c5a",
+        "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY,
       },
     });
 
